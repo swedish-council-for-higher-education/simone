e47930cfd9fcb05fb5e257c7ef9298073fc3829f
Mattias Andersson <mattias.andersson@umu.se>
2025-05-12 09:01:49 +0200

    use jaxrs @FormParam

--- simone.core/src/main/java/se/uhr/simone/core/admin/boundary/DatabaseResource.java
+++ simone.core/src/main/java/se/uhr/simone/core/admin/boundary/DatabaseResource.java
@@ -1,13 +1,16 @@
 package se.uhr.simone.core.admin.boundary;
 
 import java.util.List;
-import java.util.Map;
+
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.EntityPart;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.ParameterStyle;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -15,6 +18,7 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameters;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
 import se.uhr.simone.common.db.FileLoadResultRepresentation;
 import se.uhr.simone.core.SimOne;
 
@@ -41,16 +45,12 @@ public class DatabaseResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@POST
-	public Response load(Map<String, String> parts) {
-		if (parts.containsKey("name") && parts.containsKey("content")) {
-			FileLoadResult result = loadFile(parts.get("name"), parts.get("content"));
-			return Response.status(result.errorMessage == null ? Response.Status.OK : Response.Status.BAD_REQUEST)
-					.entity(FileLoadResultRepresentation.of(result.eventIdList, result.errorMessage))
-					.build();
+	public Response load(@FormParam("name") String name, @FormParam("content") EntityPart content) {
+		FileLoadResult result = loadFile(name, content);
+		return Response.status(result.errorMessage == null ? Response.Status.OK : Response.Status.BAD_REQUEST)
+				.entity(FileLoadResultRepresentation.of(result.eventIdList, result.errorMessage))
+				.build();
 
-		} else {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
 	}
 
 	/**
@@ -61,11 +61,11 @@ public class DatabaseResource {
 	 * @return The result of the load
 	 */
 
-	protected FileLoadResult loadFile(String name, String content) {
+	protected FileLoadResult loadFile(String name, EntityPart content) {
 		throw new UnsupportedOperationException("The endpoint exists for backwards compatibility, use a custom endpoint instead");
 	}
 
-	public static record FileLoadResult(String errorMessage, List<String> eventIdList){
+	public static record FileLoadResult(String errorMessage, List<String> eventIdList) {
 
 		public static FileLoadResult withErrorMessage(String errorMessage) {
 			return new FileLoadResult(errorMessage, List.of());
